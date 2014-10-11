cf430e52c988d8c086730f01a33513128e635495
Ching Yi, Chan <chingyichan.tw@gmail.com>
2014-10-11 12:59:21 +0800

    add a single web to accept download command

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup
 
 setup(name='ivod_downloader',
-        version='0.1',
-        install_requires=["youtube-dl>=2014.09.25", "BeautifulSoup>=3.2.1"],
-        scripts=['scripts/ivod_downloader.py', 'scripts/ivod_single_downloader.py'],
-        packages=['ivod'],
-        test_suite='tests.function_test')
+      version='0.1',
+      install_requires=["youtube-dl>=2014.09.25", "BeautifulSoup>=3.2.1"],
+      scripts=[
+          'scripts/ivod_downloader.py',
+          'scripts/ivod_single_downloader.py',
+          'scripts/ivod_schedule_download_web.py'],
+      packages=['ivod'],
+      test_suite='tests.function_test')

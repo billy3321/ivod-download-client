44731deeaf58eb8a7506478824c246bb6e30b73b
Ching Yi, Chan <chingyichan.tw@gmail.com>
2014-10-11 22:51:22 +0800

    fix broken tests

--- tests/function_test.py
+++ tests/function_test.py
@@ -2,15 +2,14 @@ import unittest
 from ivod.origin import ivod_downloader as origin_ivod_downloader
 from ivod.origin import ivod_single_downloader as origin_ivod_single_downloader
 
-import ivod_downloader
-import ivod_single_downloader
+from ivod import commands
 
 
 class TestVODResults(unittest.TestCase):
 
     def test_get_movie_by_date(self):
         origin = origin_ivod_downloader.get_movie_by_date(8, '2014-10-09')
-        current = ivod_downloader.get_movie_by_date(8, '2014-10-09')
+        current = commands.list_meeting_video_of_committee(8, '2014-10-09')
 
         self.assertTrue(current)
         self.assertEqual(origin, current)
@@ -20,21 +19,14 @@ class TestVODResults(unittest.TestCase):
             8,
             '2014-03-01',
             '2014-10-01')
-        current = ivod_downloader.get_date_list(8, '2014-03-01', '2014-10-01')
-
-        self.assertTrue(current)
-        self.assertEqual(origin, current)
-
-    def test_get_movie_by_date(self):
-        origin = origin_ivod_downloader.get_movie_by_date(8, '2014-10-01')
-        current = ivod_downloader.get_movie_by_date(8, '2014-10-01')
+        current = commands.list_meeting_date_of_committee(8, '2014-03-01', '2014-10-01')
 
         self.assertTrue(current)
         self.assertEqual(origin, current)
 
     def test_get_movie_url(self):
         origin = origin_ivod_downloader.get_movie_url('74035', 'clip')
-        current = ivod_downloader.get_movie_url('74035', 'clip')
+        current = commands.generate_video_url('74035', 'clip')
 
         self.assertTrue(current)
         self.assertEqual(origin, current)
@@ -50,11 +42,11 @@ class TestVODSingleResults(unittest.TestCase):
 
     def test_single_downloader(self):
         origin_ivod_single_downloader.download_adobe_hds = self._mock_adobe_hds_downloader
-        ivod_single_downloader.download_adobe_hds = self._mock_adobe_hds_downloader
+        commands.download_adobe_hds = self._mock_adobe_hds_downloader
 
         origin_ivod_single_downloader.download_from_url(
             'http://ivod.ly.gov.tw/Play/VOD/76394/300K')
-        ivod_single_downloader.download_from_url(
+        commands.download_single_high_quality_video(
             'http://ivod.ly.gov.tw/Play/VOD/76394/300K')
 
         self.assertTrue(self.download_url_args[0])
